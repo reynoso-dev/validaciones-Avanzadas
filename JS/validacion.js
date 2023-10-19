60efa9e602da0b1786758f0f6355df11ec64faec
Nicolas-Guarino <nicolasguarino03@gmail.com>
2023-10-19 19:45:31 -0300

    validation name

--- JS/validacion.js
+++ JS/validacion.js
@@ -1,10 +1,40 @@
-function validarContraseña (){
-  const contraseña1 = document.getElementById ("password1").value;
-  const contraseña2 = document.getElementById ("password2").value;
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.querySelector('form');
 
-if (contraseña1 === contraseña2) {
-            alert("Las contraseñas coinciden.");
-        } else {
-            alert("Las contraseñas no coinciden. Inténtalo de nuevo por favor.");
-        }  
-}
+  form.addEventListener('submit', function (event) {
+    let valid = true;
+
+    function mostrarError(elemento, mensaje) {
+      elemento.classList.add('is-invalid');
+      elemento.nextElementSibling.innerText = mensaje;
+      valid = false;
+    };
+
+    function mostrarExito(elemento) {
+      elemento.classList.remove('is-invalid');
+      elemento.classList.add('is-valid');
+      elemento.nextElementSibling.innerText = '';
+    };
+
+
+    const nombre = document.getElementById('nombre');
+    let check = 0;
+
+    
+    if (nombre.value.trim() === '') {
+      mostrarError(nombre, 'Este campo no puede estar vacio');
+    } else {
+      mostrarExito(nombre)
+      check = 1;
+    }
+
+
+
+    if (!valid) {
+      event.preventDefault();
+    }
+    if (check == 1) {
+      alert("Formulario enviado correctamente");
+    }
+  });
+});
